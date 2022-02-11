27e04afc9036fdd6a6afef616454983fd21ae9ec
clarissalimoa <clarissalimoa12@gmail.com>
2022-02-12 00:11:55 +0700

    catch pokemon, reducer, localstorage,

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { createContext } from "react";
+
+export const AppContext = createContext(null);
 
 function MyApp({ Component, pageProps }: AppProps) {
   const apolloClient = new ApolloClient({
